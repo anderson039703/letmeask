e1ccadc99c51c9a50ef34d45362c50b12dfecf10
Anderson Pinheiro <andersonfernandespinheiro@gmail.com>
2021-06-25 11:21:17 -0300

    Stage 2

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,19 @@
-import  {Button}  from './components/Button'
+import {BrowserRouter, Route} from 'react-router-dom'
+
+import { Home } from './pages/Home';
+import { NewHome } from "./pages/NewHome";
+
+import {AuthContextProvider} from './contexts/AuthContext'
 
 function App() {
+
   return (
-    <div>
-      <Button/>
-    </div>
+    <BrowserRouter>
+      <AuthContextProvider>
+        <Route path="/" exact component={Home}/>
+        <Route path="/rooms/new" component={NewHome}/>
+      </AuthContextProvider>
+    </BrowserRouter>
   );
 }
 
