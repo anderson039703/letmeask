591b852c9ec6ec26244d43944216644221705240
Anderson Pinheiro <andersonfernandespinheiro@gmail.com>
2021-08-05 10:42:46 -0300

    Stage 3

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,21 @@
-import {BrowserRouter, Route} from 'react-router-dom'
+import {BrowserRouter, Route, Switch} from 'react-router-dom'
 
 import { Home } from './pages/Home';
 import { NewHome } from "./pages/NewHome";
 
 import {AuthContextProvider} from './contexts/AuthContext'
+import { Room } from './pages/Room';
 
 function App() {
 
   return (
     <BrowserRouter>
       <AuthContextProvider>
-        <Route path="/" exact component={Home}/>
-        <Route path="/rooms/new" component={NewHome}/>
+        <Switch>
+          <Route path="/" exact component={Home}/>
+          <Route path="/rooms/new" component={NewHome}/>
+          <Route path="/rooms/:id" component={Room}/>
+        </Switch>
       </AuthContextProvider>
     </BrowserRouter>
   );
